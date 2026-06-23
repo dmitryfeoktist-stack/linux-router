a47577c5e4416e0776f95631a12112478adc7e5f
LLlkoJlbHuk <dmitryfeoktist@gmail.com>
2026-06-23 20:48:07 +0300

    Change repo

--- provision/router.sh
+++ provision/router.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -ex
 
+if [ -f /etc/centos-release ] && grep -q "CentOS Linux release 7" /etc/centos-release; then
+    echo "CentOS 7 detected, switching to vault repos..."
+    sed -i 's/mirror.centos.org/vault.centos.org/g' /etc/yum.repos.d/*.repo
+    sed -i 's/^#.*baseurl=/baseurl=/g' /etc/yum.repos.d/*.repo
+    sed -i 's/^mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/*.repo
+    yum clean all
+    yum makecache
+fi
+
+
 if command -v dnf &> /dev/null; then
     PKG_MGR="dnf"
 elif command -v yum &> /dev/null; then
